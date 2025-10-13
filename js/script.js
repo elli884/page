46d0993ecb28c9d8bea7a071cbe4f9a257e5718a
elli884 <elisabeth.nejedli@alumni.fh-aachen.de>
2025-10-13 21:05:07 +0200

    finished page

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('clickMe');
-const output = document.getElementById('output');
-
-button.addEventListener('click', () => {
-    output.textContent = "Du hast den Button geklickt!";
-});
